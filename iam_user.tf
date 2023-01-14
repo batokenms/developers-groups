0dc3971d0e57b8f8ed1a954efceb33e308f7f448
Jean <kenmogne12@gmail.com>
2023-01-13 22:46:22 -0600

    adding users into configuration file

--- iam_user.tf
+++ iam_user.tf
@@ -6,7 +6,6 @@ resource "aws_iam_user" "Jane" {
     tag-key = "tag-value"
   }
 }
-
 resource "aws_iam_user" "Christine" {
   name = "Christine-user"
   path = "/system/"
@@ -15,7 +14,6 @@ resource "aws_iam_user" "Christine" {
     tag-key = "tag-value"
   }
 }
-
 resource "aws_iam_user" "Ahmed" {
   name = "Ahmed-user"
   path = "/system/"
@@ -24,7 +22,6 @@ resource "aws_iam_user" "Ahmed" {
     tag-key = "tag-value"
   }
 }
-
 resource "aws_iam_user" "Desire" {
   name = "Desire-user"
   path = "/system/"
@@ -33,7 +30,6 @@ resource "aws_iam_user" "Desire" {
     tag-key = "tag-value"
   }
 }
-
 resource "aws_iam_user" "Ken" {
   name = "Ken-user"
   path = "/system/"
@@ -42,9 +38,176 @@ resource "aws_iam_user" "Ken" {
     tag-key = "tag-value"
   }
 }
-
 resource "aws_iam_user" "Leo" {
   name = "Leo-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Ann" {
+  name = "Ann-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Crissy" {
+  name = "Crissy-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Chelsea" {
+  name = "Chelsea-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Doe" {
+  name = "Doe-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Shamir" {
+  name = "Shamir-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Koffo" {
+  name = "Koffo-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Ipupa" {
+  name = "Ipupa-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Marie" {
+  name = "Marie-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Shayana" {
+  name = "Shayana-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Justice" {
+  name = "Justice-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Samuel" {
+  name = "Samuel-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Water" {
+  name = "Water-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Cambiasso" {
+  name = "Cambiasso-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Gatuso" {
+  name = "Gatuso-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Shakeem" {
+  name = "Shakeem-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Keajah" {
+  name = "Keajah-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Melissa" {
+  name = "Melissa-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Mogne" {
+  name = "Mogne-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Rose" {
+  name = "Rose-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "David" {
+  name = "David-user"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+resource "aws_iam_user" "Caren" {
+  name = "Caren-user"
   path = "/system/"
 
   tags = {
